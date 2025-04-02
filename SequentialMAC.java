61cf4441b79f62dedcb7162f6c07af7c004bf523
Pauline Kang <74037846+tae-1230@users.noreply.github.com>
2025-04-02 11:29:48 -0700

    test
    matt

--- SequentialMAC.java
+++ SequentialMAC.java
@@ -2,7 +2,8 @@ package mac;
 import java.util.*;
 
 public class SequentialMAC {
-    public int compute(int[] x, int[] y) {
+
+    public static int compute(int[] x, int[] y) {
         int result = 0;
         for (int i = 0; i < x.length; i++) {
             result += x[i] * y[i];
